fac31d621248773a6ec240db33d91148451ca90a
AadelAferyad <animevineu@gmail.com>
2023-11-28 16:19:09 +0100

    comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        print("{}{}, ".format(i, j), end="")
-print("")
+
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
