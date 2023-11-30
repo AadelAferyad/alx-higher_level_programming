95005eaaee1b8b823e275405e6228b3f795958d8
AadelAferyad <animevineu@gmail.com>
2023-11-30 18:49:38 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
-    if (argc < 2):
-        print("{} arguments.".format(argc - 1))
+    if (argc - 1) == 0:
+        print("0 arguments.")
     else:
         print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
