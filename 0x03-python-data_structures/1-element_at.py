61ce8713d1e92fe9d743f2e2489e2e0e2bf38dc8
AadelAferyad <animevineu@gmail.com>
2023-12-03 16:00:08 +0100

    reduce the code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,6 @@
 
 def element_at(my_list, idx):
     length = len(my_list)
-    if (idx < 0):
-        return ("None")
-    if (idx > length or length == 0):
+    if (idx < 0 or idx >= length or length == 0):
         return ("None")
     return (my_list[idx])
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     length = len(my_list)
-    if (idx < 0):
-        return (my_list)
-    if (idx > length or length == 0):
+    if (idx < 0 or idx >= length or length == 0):
         return (my_list)
     my_list[idx] = element
     return (my_list)
