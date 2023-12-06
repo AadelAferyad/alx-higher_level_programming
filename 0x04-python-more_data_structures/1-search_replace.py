67b116e2077c188b1908094f1f5b0e1bdc9ceabc
AadelAferyad <animevineu@gmail.com>
2023-12-06 19:05:30 +0100

    task-1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if my_list is None or search >= len(my_list):
+    if my_list is None:
         return None
-
+    if search >= len(my_list):
+        return my_list
     new = []
     for i in range(len(my_list)):
         if search - 1 == i:
