bcc4970ae1910650ef28304d93ca5f12c41096ae
AadelAferyad <animevineu@gmail.com>
2023-12-06 19:02:15 +0100

    task-1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
+    new = []
+    for i in range(len(my_list)):
+        if search - 1 == i:
+            new.append(replace)
+        else:
+            new.append(my_list[i])
+    return new
