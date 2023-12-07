3143809e19c2902c39e7cf685a7ca35430e359e5
AadelAferyad <animevineu@gmail.com>
2023-12-07 01:14:17 +0100

    list tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,11 @@
 def weight_average(my_list=[]):
     if my_list is None:
         return None
-    multi = 1
+    multi = 0
     al = 0
     for i in my_list:
         al += i[0] * i[1]
         multi += i[1]
+    if multi == 0:
+        multi = 1
     return al / multi
