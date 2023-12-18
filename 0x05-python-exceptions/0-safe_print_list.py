65c32db34af3e9e94dde0cd0bdf744a5a0468c73
AadelAferyad <animevineu@gmail.com>
2023-12-18 10:54:00 +0100

    except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,8 @@ def safe_print_list(my_list=[], x=0):
         while my_list[i] and (i) != x:
             print("{}".format(my_list[i]), end="")
             i += 1
+    except Exception as e:
+        pass
+    if i != 0:
         print()
-    except booo:
-        print()
-        return (i)
     return (i)
