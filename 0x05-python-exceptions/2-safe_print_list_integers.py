eb6b42f1fbbd120b60b94a625c105a003d1d8ed3
AadelAferyad <animevineu@gmail.com>
2023-12-18 12:43:20 +0100

    safe?

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,8 +2,8 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-        i = 0
-        j = 0
+    i = 0
+    j = 0
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
