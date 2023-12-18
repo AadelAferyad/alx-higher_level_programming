d507be03ac9b86bfd4e599b786fa036b8e9b9640
AadelAferyad <animevineu@gmail.com>
2023-12-18 12:59:20 +0100

    safe division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,9 +2,7 @@
 
 def safe_print_division(a, b):
     try:
-        r = 0
-        if b != 0:
-            r = a / b
+        r = a / b
     except ZeroDivisionError:
         r = None
     finally:
