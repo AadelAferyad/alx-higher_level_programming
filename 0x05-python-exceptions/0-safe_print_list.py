f6a8ffc6de567fd0ba260050396968a97ae7d5cb
AadelAferyad <animevineu@gmail.com>
2023-12-18 10:39:04 +0100

    Try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         i = 0
         while my_list[i] and (i) != x:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             i += 1
         print()
     except booo:
