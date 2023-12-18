2c29e0caff579e9a187b6181b6d23aa9cae6070d
AadelAferyad <animevineu@gmail.com>
2023-12-18 11:00:12 +0100

    except pass

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         i = 0
-        while my_list[i] and (i) != x:
+        while my_list[i] and (i) < x:
             print("{}".format(my_list[i]), end="")
             i += 1
     except Exception as e:
