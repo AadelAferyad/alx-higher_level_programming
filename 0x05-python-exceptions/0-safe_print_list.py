35d669a3b58384793c4a90fcc80710224730afbd
AadelAferyad <animevineu@gmail.com>
2023-12-18 11:06:36 +0100

    except pass finally

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,14 +12,3 @@ def safe_print_list(my_list=[], x=0):
         if i != 0:
             print()
     return (i)
-
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(None, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) - 100)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
