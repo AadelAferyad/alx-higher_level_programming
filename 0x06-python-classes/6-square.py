c4619585dfc386302037485b2ab0eefe90034007
AadelAferyad <animevineu@gmail.com>
2023-12-19 22:29:55 +0100

    Lamp

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" creating new class """
+"""class square"""
 
 
 class Square:
@@ -8,28 +8,25 @@ class Square:
         self.__size = size
         self.__position = position
 
-    def area(self):
-        """ return square area """
-        return (self.__size * self.__size)
-
     @property
     def size(self):
         """ getter """
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
         """ setter """
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        elif (value < 0):
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
 
     @property
     def position(self):
         """ getter """
-        return (self.__position)
+        return self.__position
 
     @position.setter
     def position(self, value):
@@ -40,18 +37,17 @@ class Square:
             self.__position = value
         else:
             raise TypeError('position must be a tuple of 2 positive integers')
-        self.__position = value
 
     def my_print(self):
-        """ print '#' size times """
+        """Prints '#' """
         if self.__size == 0:
-            print("")
+            print()
         else:
-            for j in range(self.__position[1]):
-                print()
+            for new_line in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
-                for s in range(self.__position[0]):
+                for space in range(self.__position[0]):
                     print(" ", end="")
-                for line in range(self.__size):
+                for dyaz in range(self.__size):
                     print("#", end="")
                 print("")
