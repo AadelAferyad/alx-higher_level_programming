2ce23a4161a2461bbb4ebdc4fa14bcd58d99b7d5
AadelAferyad <animevineu@gmail.com>
2023-12-20 15:58:24 +0100

    __eq__

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python3
-"""class """
+""" class """
 
 
 class Square:
-    """class"""
+    """ class def """
     def __init__(self, size=0):
         self.__size = size
+
+    def area(self):
+        """ return square area """
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """ getter """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ setter """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, obj):
+        """ equal """
+        return (self.size == obj.size)
+
+    def __lt__(self, obj):
+        """ less then """
+        return (self.size < obj.size)
+
+    def __le__(self, obj):
+        """ less and equal then """
+        return (self.size <= obj.size)
