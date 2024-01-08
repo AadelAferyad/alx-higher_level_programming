56d8c15310179268280f330d30cec348727ba3c1
AadelAferyad <animevineu@gmail.com>
2024-01-08 18:48:50 +0100

    not the same class but is instance of another class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ or indirectly) from the specified class ; otherwise False.
 
 def inherits_from(obj, a_class):
     """ the function """
-    return (isinstance(obj, a_class))
+    return (isinstance(obj, a_class) and not type(obj, a_class))
