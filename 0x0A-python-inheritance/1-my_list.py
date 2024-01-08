c9b79d2082dcaddf2361d61312d098d6231b738e
AadelAferyad <animevineu@gmail.com>
2024-01-08 18:27:12 +0100

    my class inherits from the list class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@ class MyList that inherits fr list
 
 class MyList(list):
     """ class MyList """
-    def __init__(self):
-        """ constructor """
-        super().__init__()
-
     def print_sorted(self):
         """ print sorted list """
         tmp = self.copy()
