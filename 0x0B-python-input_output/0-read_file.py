a0ca287145dd41a03db4a06f3ce20a3ebfa027d2
AadelAferyad <animevineu@gmail.com>
2024-01-09 19:16:47 +0100

    read from a file whit with keyword

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """ open a file and read it's content """
-    if filename is not None:
-        with open(filename, 'r', encoding="utf-8") as a:
-            print(a.read())
+    with open(filename, 'r', encoding="utf-8") as a:
+        print(a.read())
