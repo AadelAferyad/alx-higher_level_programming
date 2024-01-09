be947b7e4db8420fdb059e894421a10dddfa51c9
AadelAferyad <animevineu@gmail.com>
2024-01-09 20:18:34 +0100

    add objects to file.json

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" json """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that writes an Object to a text file,
+    using a JSON representation"""
+    with open(filename, "w", encoding="utf-8") as fd:
+        return fd.write(json.dumps(my_obj))
