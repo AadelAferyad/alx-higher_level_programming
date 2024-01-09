95099b415512592cbcd05553c09e514368760a25
AadelAferyad <animevineu@gmail.com>
2024-01-10 00:41:53 +0100

    __dict__

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" back to class """
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary description with simple
+    data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object"""
+    return obj.__dict__
