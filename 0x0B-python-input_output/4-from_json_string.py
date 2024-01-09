56b95abb82b59cf8c811d1ad923e0f60f9343a19
AadelAferyad <animevineu@gmail.com>
2024-01-09 20:03:47 +0100

    json.loads

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+
+
+def from_json_string(my_str):
+    """ function that returns an object
+    (Python data structure) represented by a JSON string"""
+    return (json.loads(my_str))
