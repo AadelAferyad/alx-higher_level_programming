93198f3d4378f2e624c8988e892ed38629ba14b2
AadelAferyad <animevineu@gmail.com>
2024-01-15 20:46:00 +0100

    static method added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ first class everything"""
+import json
 
 
 class Base:
@@ -13,3 +14,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ to json string """
+        s = "[]"
+        if list_dictionaries is None:
+            return (s)
+        return (json.dumps(list_dictionaries))
