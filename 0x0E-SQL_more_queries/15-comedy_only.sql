4ef8b2ee22a169531b5f6d83c25d839d2b10909b
AadelAferyad <animevineu@gmail.com>
2024-02-15 02:27:53 +0100

    you can make alise when importing (FROM) or (JOIN)

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT sh.title FROM tv_shows sh JOIN tv_show_genres tsg ON sh.id = tsg.show_id JOIN tv_genres tg ON tg.id = tsg.genre_id WHERE tg.name = "Comedy" ORDER BY sh.title ASC; 
