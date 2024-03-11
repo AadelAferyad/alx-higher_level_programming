12e78e935024345dc2eef4b034bfa2fb34c97001
AadelAferyad <animevineu@gmail.com>
2024-03-11 22:27:06 +0000

    argc and argv in js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,9 @@
 const args = process.argv.slice(2);
 const argc = args.length;
 
-if (argc > 0) {
+if (argc == 1) {
+  console.log('Argument found');
+} else if (argc > 1) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
