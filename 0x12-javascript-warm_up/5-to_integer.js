22f8e21454646a3a62a3e286d4b58b6e99b8bcef
AadelAferyad <animevineu@gmail.com>
2024-03-11 23:22:51 +0000

    to int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const argv = process.argv.slice(2);
-if (!argv[0]) {
+if (!argv[0] || isNaN(argv[0])) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + argv[0]);
