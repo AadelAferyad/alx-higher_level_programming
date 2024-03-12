48ab511b1dae470edb1258dcfc9ce066f2dae958
AadelAferyad <animevineu@gmail.com>
2024-03-12 00:29:09 +0000

    second biggest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const argv = process.argv.slice(2);
+const argc = argv.length;
+let i;
+let j;
+let temp;
+if (argc < 2) {
+  console.log(1);
+} else {
+  for (i = 0; i < argc; i++) {
+    for (j = i + 1; j < argc; j++) {
+      if (parseInt(argv[i]) > parseInt(argv[j])) {
+        temp = parseInt(argv[i]);
+        argv[i] = parseInt(argv[j]);
+        argv[j] = temp;
+      }
+    }
+  }
+  console.log(argv[argc - 2]);
+}
