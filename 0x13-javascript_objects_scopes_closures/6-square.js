277543cb95ffbb81ddff72031cb5c9ddf9ca62cb
AadelAferyad <animevineu@gmail.com>
2024-03-12 17:25:35 +0000

    print square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c) {
+      let i;
+      let j;
+      let message = c;
+      for (j = 0; j < this.size; j++) {
+        message += c;
+      }
+      for (i = 0; i < this.size; i++) {
+        console.log(message);
+      }
+    } else {
+      super.print();
+    }
+  }
+}
+module.exports = Square;
