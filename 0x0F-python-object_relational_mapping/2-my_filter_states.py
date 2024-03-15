706de6a859118dd09be7e0a8b6f43d2faaafb8d1
AadelAferyad <animevineu@gmail.com>
2024-03-15 12:56:03 +0000

    filter using sql queryes

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This file is for a script that establish a sql connection
+and fetch data from a database
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__" and len(argv) == 5:
+    """
+    script that takes in an argument and displays
+    all values in the states table of hbtn_0e_0_usa
+    where name matches the argument.
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         password=argv[2], database=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4], ))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    db.close()
