84185c983ba1653de137c2fcbc1251ab4b816b65
AadelAferyad <animevineu@gmail.com>
2024-04-09 12:44:29 +0000

    bash script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 #size of the downloaded body in bytes
-if [ $# -eq 1 ];then curl -s -o /dev/null -w "%{size_download}" $1;echo "";fi
+curl -s -o /dev/null -w "%{size_download}" $1;echo ""
