5677d4fa66684887303f2e392239a826a5e79a1b
AadelAferyad <animevineu@gmail.com>
2024-04-14 17:27:51 +0100

    post requ

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #send a post message using curl
-curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
+curl -X POST -d email=test@gmail.com -d subject="I will always be here for PLD" $1
