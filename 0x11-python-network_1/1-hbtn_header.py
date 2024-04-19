aa1fde900b4346dbf37c8fb81e1c0907623a93b7
AadelAferyad <animevineu@gmail.com>
2024-04-19 19:22:51 +0100

    display X-Request-Id of request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""send a get requests and display the X-Request-Id"""
 from sys import argv
 import urllib.request
-
 if len(argv) > 1:
     with urllib.request.urlopen(argv[1]) as res:
         body = res.getheaders()
