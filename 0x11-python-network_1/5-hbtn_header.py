2ee578174dc1e5f9849039800a4183a34ed2f715
AadelAferyad <animevineu@gmail.com>
2024-04-20 02:03:33 +0100

    headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+20/4/2024 2am blati mix, high hamdolilah
+alx almost done , fin ana wtf fin ghadi chkon ana
+"""
+from requests import get
+from sys import argv as av
+
+if len(av) > 1:
+    r = get(av[1])
+    print(r.headers['X-Request-Id'])
