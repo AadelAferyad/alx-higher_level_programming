98479913904d8163492db537c219ed66647e1e03
AadelAferyad <animevineu@gmail.com>
2024-04-20 02:04:49 +0100

    special moment

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,7 +5,6 @@ alx almost done , fin ana wtf fin ghadi chkon ana
 """
 from requests import get
 from sys import argv as av
-
 if len(av) > 1:
     r = get(av[1])
     print(r.headers['X-Request-Id'])
