be4dca3831b8c86954f1c20211aa773116df1704
AadelAferyad <animevineu@gmail.com>
2024-04-20 02:16:54 +0100

    special moment

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,8 +3,8 @@
 20/4/2024 2am blati mix, high hamdolilah
 alx almost done , fin ana wtf fin ghadi chkon ana
 """
-
-from requests import get
-from sys import argv as av
-r = get(av[1])
-print(r.headers['X-Request-Id'])
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv as av
+    r = get(av[1])
+    print(r.headers['X-Request-Id'])
