c9c552483028e1c56af8708c314eb0a6da8e3a50
AadelAferyad <animevineu@gmail.com>
2024-04-20 12:23:53 +0100

    send post request using requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,10 +2,10 @@
 """ send a post request using requests module """
 from sys import argv as av
 from requests import get, post
-
-url = av[1]
-data = {'email': av[2]}
-p = post(url, data=data)
-req = get(url)
-print(p.text)
-print(req.text)
+if __name__ == '__main__':
+    url = av[1]
+    data = {'email': av[2]}
+    p = post(url, data=data)
+    req = get(url)
+    print(p.text)
+    print(req.text)
